b1858215628a6ee933731af72f3e75ac85cb6ef0
tyler carson <tyler.c.carson@gmail.com>
2017-11-02 15:47:53 -0700

    add sat test 1 key

---
+++ client/src/data/exampleExamData.js
@@ -0,0 +1,25 @@
+window.sat_1_key = {
+	reading: [
+    B, B, C, A, C, D, D, B, C, B, A,
+    B, D, A, A, C, C, D, A, B, A, B,
+    D, D, C, B, D, C, A, A, D, B, A, 
+    C, B, D, C, C, B, C, B, B, A, A,
+    D, C, B, A, D, B, D, A
+  ],
+	writing: [
+		D, B,	A, C,	C, D,	B, C,	A, A,	B,
+		B, A, B, C, C, C, A, D, D, B, D,
+		D, D, B, A, B, C, B, D, C, A, A,
+		A, A, B, D, C, A, B, B, C, D, D
+  ],
+  math_no_calc: [
+    D, A, C, B, C, A, B, C, B, A,
+    D, D, B, A, D, 2, 1600, 7, 0.8, 100
+  ],
+  math_calc: [
+    B, C, D, C, D, D, C, D, A, B,
+    A, C, C, C, A, C, B, A, B, D,
+    C, B, B, A, D, B, C, C, D, D, 
+    5, 107, 0.625, 96, 6, 3, 1.02, 6.11
+  ]
+}
